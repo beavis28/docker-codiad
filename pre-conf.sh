5ccf33dc9c121281864f5e33d28c2a4adc1761fb
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2015-09-27 09:16:21 -0400

    Create pre-conf.sh

---
+++ pre-conf.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+#download codiad 
+cd /var/www
+git clone https://github.com/Codiad/Codiad.git
+
+ #apache2 conf
+ a2enmod rewrite
+ chown -R www-data:www-data /var/www /var/log/apache2
+ sed -i "s/allow_url_fopen = Off/allow_url_fopen = On/" /etc/php5/apache2/php.ini
+ sed -i "s/newrelic.enabled = On/newrelic.enabled = Off/" /etc/php5/apache2/php.ini
+ sed -i "s/error_reporting = .*$/error_reporting = E_ERROR | E_WARNING | E_PARSE/" /etc/php5/apache2/php.ini
+ rm -R /var/www/html/
+ 
+ #to fix error relate to ip address of container apache2
+ echo "ServerName localhost" | tee /etc/apache2/conf-available/fqdn.conf
+ ln -s /etc/apache2/conf-available/fqdn.conf /etc/apache2/conf-enabled/fqdn.conf
