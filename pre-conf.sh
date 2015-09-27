50af155d81dba38d9a6c4e806c56a195c45d3b26
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2015-09-27 10:18:49 -0400

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -7,7 +7,8 @@ git clone https://github.com/Codiad/Codiad.git
 
  #apache2 conf
  a2enmod rewrite
- chown -R www-data:www-data /var/www /var/log/apache2
+ chown -R www-data:www-data /var/www/Codiad /var/log/apache2
+ sed -i 's/DocumentRoot \/var\/www\/html/DocumentRoot \/var\/www\/Codiad/' /etc/apache2/sites-enabled/000*.conf
  sed -i "s/allow_url_fopen = Off/allow_url_fopen = On/" /etc/php5/apache2/php.ini
  sed -i "s/newrelic.enabled = On/newrelic.enabled = Off/" /etc/php5/apache2/php.ini
  sed -i "s/error_reporting = .*$/error_reporting = E_ERROR | E_WARNING | E_PARSE/" /etc/php5/apache2/php.ini
